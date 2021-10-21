19d8568be5ffe807b6d501e68650e3a04a273e58
Valdionor Junior <valdionorjunior@outlook.com>
2021-10-21 14:44:30 -0300

    [UPDATE] - call request inter MS for Feing Client

--- hr-payroll/src/main/java/com/microservice/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/services/PaymentService.java
@@ -2,32 +2,19 @@ package com.microservice.hrpayroll.services;
 
 import com.microservice.hrpayroll.entities.Payment;
 import com.microservice.hrpayroll.entities.Worker;
+import com.microservice.hrpayroll.feignclients.WorkerFeignClients;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")//buscando valor da variavel de ambiente no properties
-    private String workerHost;
-
-    @Autowired //injeta a instalcia pra disponibilizar pora consumir de outros MS
-    private RestTemplate restTemplate;
+    @Autowired
+    private WorkerFeignClients workerFeignClients;
 
     public Payment getPayment( long workerId, int days){
-        String url = workerHost.concat("/workers/{id}");
-        //criando mapara de parametro para request http
-        Map<String, String> uriVariables = new HashMap<>();
-        //chave/valor do objeto parametro
-        uriVariables.put("id", String.valueOf(workerId));
 
-        //realizando a request pro outrara o ms worker usando restTemplate
-        Worker worker = restTemplate.getForObject(url, Worker.class, uriVariables);
+        Worker worker = workerFeignClients.findById(workerId).getBody();
 
         return new Payment(worker.getName(),worker.getDailyIncome(), days );
     }
