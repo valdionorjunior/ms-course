ee264f6429ac255e05ed9cdf4742e36d787be222
Valdionor Junior <valdionorjunior@outlook.com>
2021-12-06 18:14:33 -0300

    [UPDATE] - Cors configuration

--- hr-api-gateway-zuul/src/main/java/com/microservice/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/microservice/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -1,13 +1,22 @@
 package com.microservice.hrapigatewayzuul.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
@@ -32,5 +41,33 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
             .antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR", "ADMIN") //atoriza para mais de um perfil, o metodo get
             .antMatchers(ADMIN).hasRole("ADMIN")//autoriza todas as rodas da string para perfil de ADMIN
             .anyRequest().authenticated();//qualquer outra request pode acessar porem precisa ser autenticada
+
+        http.cors().configurationSource(corsConfigurationSource());//chamando a configuração de cors
+    }
+
+
+    //BEAN PRA CONFIGURAÇÂO DE CORS
+    @Bean
+    public CorsConfigurationSource  corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*")); //permição de origens => qualquer origen
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET","PUT", "DELETE", "PATCH"));// quai metodos vou permitir
+        corsConfig.setAllowCredentials(true);// vou permitir credenciais? SIM
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));//quais heades, cabeçalhos vou permitir,
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig); //registar o cors configurado para o path selecionado, no caso tudo
+
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterFilter(){ //filtro de cors, que será executado com alta precedencia
+        FilterRegistrationBean<CorsFilter> bean
+                = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);//executado com alta precedencia
+
+        return bean;
     }
 }
